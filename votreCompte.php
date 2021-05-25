541e64e57cfb48d62b7cb285a8f81f8c071fae2e
Alexandre92700 <alexandre.deoliveirabaptista@edu.ece.fr>
2021-05-25 19:07:17 +0200

    navbar-light

--- votreCompte.php
+++ votreCompte.php
@@ -43,7 +43,7 @@ session_start();
 		<!--NAVIGATION-->
 		<div class="navigation container">
 			<div class="row">
-				<nav class="col navbar navbar-expand-lg">
+				<nav class="col navbar navbar-expand-lg navbar-light">
 					 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" name="MENU"><span class="navbar-toggler-icon"></span>
 	   				</button>
 	   				<div id="navbarContent" class="collapse navbar-collapse navbar-light">
